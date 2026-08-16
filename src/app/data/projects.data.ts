880d98166bced999fbd27a314f8de97c0b339121
Marwen Feki <marwenfeki214@gmail.com>
2026-08-16 11:34:58 +0200

    Final touches

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -18,6 +18,7 @@ export const PROJECTS: ProjectItem[] = [
     technologies: ['OpenStack', 'Kubernetes', 'Ansible', 'Docker', 'Spring Boot', 'Angular', 'MySQL', 'Heat Templates', 'Infrastructure-as-Code', 'Linux', 'Bash Scripting'],
     tags: ['OpenStack', 'Kubernetes', 'Ansible', 'Docker'],
     promoVideo: 'https://drive.google.com/uc?export=view&id=1P7ENrL93BSz1W54Mcfa_ES2OQR9ORRmL',
+    logo: 'assets/images/turathai-logo.png',
     demoLink: 'https://drive.google.com/uc?export=view&id=1mpNo1r0Dgdzie2J5L6n5BuyEHXyilaXJ',
     repos: { frontend: 'https://github.com/MarwenDev2/TurathAI-Frontend', backend: 'https://github.com/MarwenDev2/TurathAI-Backend' },
     image: ['assets/images/projects/turathAI.png']
@@ -27,6 +28,7 @@ export const PROJECTS: ProjectItem[] = [
     description: 'Developed a comprehensive HR management platform using Spring Boot and Angular, deployed on a private VPS with Docker containerization. Implemented a multi-container setup with Docker Compose and configured Nginx reverse proxy with TLS termination, improving request routing efficiency by 25%. Automated backend deployment scripts on Linux servers, reducing manual deployment effort by over 30% while enhancing system stability and deployment reliability.',
     technologies: ['Angular', 'Spring Boot', 'MySQL', 'Docker', 'Docker Compose', 'Nginx', 'Linux', 'REST API', 'Bootstrap'],
     tags: ['Angular', 'Spring Boot', 'Docker', 'MySQL'],
+    logo: 'assets/images/unitedservices-logo.png',
     image: [
       'assets/images/projects/rh1.png',
       'assets/images/projects/rh2.png',
@@ -43,6 +45,7 @@ export const PROJECTS: ProjectItem[] = [
     technologies: ['Java', 'JavaFX', 'Symfony', 'PHP', 'MySQL', 'Agile Methodology', 'System Integration', 'Testing'],
     tags: ['Java', 'Symfony', 'MySQL'],
     demoLink: 'https://drive.google.com/uc?export=view&id=1puWul_b_0_91oThe_nj5PtNcyPKiUXrL',
+    logo: 'assets/images/matchmate-logo.png',
     githubLink: 'https://github.com/MarwenDev2/MatchMate-Symfony',
     image: [
       'assets/images/projects/matchmate1.jpg',
